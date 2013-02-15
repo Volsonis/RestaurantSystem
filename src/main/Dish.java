20fe3d330a249a26ac6032eaadd1bd66ec7cea5f
Stefan Lesnjakovic <stefan_lesnjakovic@msn.com>
2013-02-15 18:55:37 +0000

    Initial commit: created dish class

---
+++ src/main/Dish.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Dish
+{
+	private int ID;
+	private String name;
+	private String descripton;
+	private double price;
+	
+	public Dish(int iD, String name, String descripton, double price)
+	{
+		super();
+		ID = iD;
+		this.name = name;
+		this.descripton = descripton;
+		this.price = price;
+	}
+	
+	
+}
