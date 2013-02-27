2be6ee7ea8d4e0959cfc889da262727f978e9d13
Stefan Lesnjakovic <stefan_lesnjakovic@msn.com>
2013-02-27 16:44:33 +0000

    added some main classes

--- src/main/Dish.java
+++ src/main/Dish.java
@@ -15,6 +15,35 @@ public class Dish
 		this.descripton = descripton;
 		this.price = price;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescripton() {
+		return descripton;
+	}
+
+	public void setDescripton(String descripton) {
+		this.descripton = descripton;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getID() {
+		return ID;
+	}
+	
 	
 	
 }
